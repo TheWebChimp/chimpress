893e35b1b03ac4641e534f5a811ff8667b7cb832
webchimp <rodrigo.tejero@thewebchi.mp>
2014-07-26 12:50:02 -0500

    Sticky Footer added

--- parts/shared/footer.php
+++ parts/shared/footer.php
@@ -1,4 +1,8 @@
-	
+	<?php if(STICKY_FOOTER): ?>
+		<div class="push"></div>
+	</div><!-- #wrapper -->
+	<?php endif; ?>
+
 	<footer>
 		&copy; <?php echo date("Y"); ?> <?php bloginfo( 'name' ); ?>. All rights reserved.
 	</footer>
--- parts/shared/header.php
+++ parts/shared/header.php
@@ -1,5 +1,9 @@
+<?php if(STICKY_FOOTER): ?>
+	<div id="wrapper">
+<?php endif; ?>
+
 <header>
 	<h1><a href="<?php echo home_url(); ?>"><?php bloginfo( 'name' ); ?></a></h1>
-	<?php bloginfo( 'description' ); ?>
-	<?php get_search_form(); ?>
+	<nav class="primary-navigation"><?php wp_nav_menu( array('container' => false, 'theme_location' => 'primary')); ?></nav>
+	<?php //get_search_form(); ?>
 </header>
